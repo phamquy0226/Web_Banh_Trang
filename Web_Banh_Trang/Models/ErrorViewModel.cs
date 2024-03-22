30c6fb4c61cac958692fe6320b14660f130a7b54
Administrator <Administrator@21AK22-COM>
2024-03-22 22:06:14 +0700

    Add project files.

---
+++ Web_Banh_Trang/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Banh_Trang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
